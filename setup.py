096137bad6cbac77348c48a72711fba87190e785
noam finkelstein <noam.finkelstein@gmail.com>
2017-11-14 20:00:08 -0500

    Rename cause dgraph is a startup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dgraph',
-    version='0.0.6',
-    description='Simple dependency graph for python functions',
-    url='https://github.com/n-s-f/dgraph',
+    name='taskmap',
+    version='0.0.1',
+    description='Dependency graph with parallel asyncronous task runner',
+    url='https://github.com/n-s-f/taskmap',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     install_requires=['multiprocess'],
 )
