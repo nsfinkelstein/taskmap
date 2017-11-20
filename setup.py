3eaa85b3bd5ad3bea741114021922afb20a1ea72
noam finkelstein <noam.finkelstein@gmail.com>
2017-11-20 13:36:09 -0500

    Bump version for logging fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='taskmap',
-    version='0.0.2',
+    version='0.0.4',
     description='Dependency graph with parallel asyncronous task runner',
     url='https://github.com/n-s-f/taskmap',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
